f16d1d13c5619a71a59e2687bf727fd91e5923db
gianvittorio <gianvittorio.castellano@gmail.com>
2025-07-03 22:23:02 -0300

    solve race condition in store

--- build_an_application/src/store/in_memory_player_store.go
+++ build_an_application/src/store/in_memory_player_store.go
@@ -1,18 +1,26 @@
 package store
 
-//in_memory_player_store.go
+import "sync"
+
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
+	return &InMemoryPlayerStore{store: map[string]int{}, mutex: sync.Mutex{}}
 }
 
 type InMemoryPlayerStore struct {
 	store map[string]int
+	mutex sync.Mutex
 }
 
 func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.mutex.Lock()
+	defer i.mutex.Unlock()
+
 	i.store[name]++
 }
 
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.mutex.Lock()
+	defer i.mutex.Unlock()
+
 	return i.store[name]
 }
--- build_an_application/test/server/integration/integration_test.go
+++ build_an_application/test/server/integration/integration_test.go
@@ -3,6 +3,7 @@ package integration_test
 import (
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
 
 	"example.com/build_an_application/src/server"
@@ -14,10 +15,18 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	webserver := server.PlayerServer{store}
 	player := "Pepper"
 
-	webserver.ServeHTTP(httptest.NewRecorder(), server.NewPostWinRequest(player))
-	webserver.ServeHTTP(httptest.NewRecorder(), server.NewPostWinRequest(player))
-	webserver.ServeHTTP(httptest.NewRecorder(), server.NewPostWinRequest(player))
+	
+	groupSize := 3
+	var wg sync.WaitGroup
+	wg.Add(groupSize)
+	for range groupSize {
+		go func() {
+			webserver.ServeHTTP(httptest.NewRecorder(), server.NewPostWinRequest(player))
+			wg.Done()
+		}()
+	}
 
+	wg.Wait()
 	response := httptest.NewRecorder()
 	webserver.ServeHTTP(response, server.NewGetScoreRequest(player))
 	server.AssertStatus(t, response.Code, http.StatusOK)
