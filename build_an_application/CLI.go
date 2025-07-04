5e4f157888f6c602db2a92aa2ebb0d4927305f2d
gianvittorio <gianvittorio.castellano@gmail.com>
2025-07-04 18:48:36 -0300

    done with cli and pkg structure

---
+++ build_an_application/CLI.go
@@ -0,0 +1,33 @@
+package poker
+
+import (
+	"bufio"
+	"io"
+	"strings"
+)
+
+type CLI struct {
+	PlayerStore PlayerStore
+	In *bufio.Scanner
+}
+
+func NewCLI(store PlayerStore, in io.Reader) *CLI {
+	return &CLI{
+		PlayerStore: store,
+		In:          bufio.NewScanner(in),
+	}
+}
+
+func (cli *CLI) PlayPoker() {
+	userInput := cli.readLine()
+	cli.PlayerStore.RecordWin(extractWinner(userInput))
+}
+
+func extractWinner(userInput string) string {
+	return strings.Replace(userInput, " wins", "", 1)
+}
+
+func (cli *CLI) readLine() string {
+	cli.In.Scan()
+	return cli.In.Text()
+}
--- build_an_application/server_test.go
+++ build_an_application/server_test.go
@@ -6,26 +6,6 @@ import (
 	"testing"
 )
 
-type StubPlayerStore struct {
-	scores   map[string]int
-	winCalls []string
-	league   League
-}
-
-func (s *StubPlayerStore) GetLeague() League {
-	return s.league
-}
-
-func (s *StubPlayerStore) GetPlayerScore(name string) int {
-	score := s.scores[name]
-	return score
-}
-
-func (s *StubPlayerStore) RecordWin(name string) {
-	s.winCalls = append(s.winCalls, name)
-}
-
-// server_test.go
 func TestGETPlayers(t *testing.T) {
 	store := StubPlayerStore{
 		map[string]int{
